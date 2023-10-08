3dd1beaefd9b0c7f62c0ef645ba3b9115ae45cd0
mhkb2216 <146079322+mhkb2216@users.noreply.github.com>
2023-10-08 22:23:02 +0500

    Update Code

--- src/CurrentDateTimeApp.java
+++ src/CurrentDateTimeApp.java
@@ -1,5 +1,18 @@
-public class Main {
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class CurrentDateTimeApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Get the current date and time
+        Date currentDateAndTime = new Date();
+
+        // Create a SimpleDateFormat object to format the date and time
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        // Format the current date and time as a string
+        String formattedDateTime = dateFormat.format(currentDateAndTime);
+
+        // Display the formatted date and time
+        System.out.println("Current Date and Time: " + formattedDateTime);
     }
 }
